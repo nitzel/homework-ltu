f701ad8b4bd2d3d652418eb15672ae14b338ade7
nitzel <jan.s.92@web.de>
2015-05-20 16:18:16 +0200

    dl: RAI comments added

--- declarative-languages/prolog/reversi.pl
+++ declarative-languages/prolog/reversi.pl
@@ -5,54 +5,52 @@ non_member(Arg,[Arg|_]) :- !,fail.
 non_member(Arg,[_|Tail]) :- !,non_member(Arg,Tail).
 non_member(_,[]).
 
+% change colors from white to black or the other way around
 swap(white,black).
 swap(black,white).
 
-% move on playground (black/white, a-h, 1-8)
+% is the move on playground (values in range: black/white, a-h, 1-8)
 position(P,X,Y) :- member(P, [white,black]) , member(X, [a,b,c,d,e,f,g,h]), member(Y, [1,2,3,4,5,6,7,8]).
 position([P,X,Y]) :- position(P,X,Y).
 
 % check if the position is empty
 posempty(Color, Board, X, Y) :- position(Color, X, Y) , non_member((_, X, Y), Board). % position empty
 
-% neighbors 
-nb(X,Y, Nx, Ny, Dx, Dy) :- member(Dx,[-1,0,1]),               % try different offsets (left,middle,right) 
-                           nth0(Px, [a,b,c,d,e,f,g,h], X),    % position of X coord in the list
+% neighbors: get the coordinates of a neighbor and also the direction Dx/Dy
+nb(X,Y, Nx, Ny, Dx, Dy) :- member(Dx,[-1,0,1]),               % try different X-offsets (left,middle,right) 
+                           nth0(Px,  [a,b,c,d,e,f,g,h], X),   % position of X coord in the list
                            Pxe is Px+Dx,                      % increase/decrease/keep as it is
                            nth0(Pxe, [a,b,c,d,e,f,g,h], Nx),  % neighbors X coordinate
-                           member(Dy,[-1,0,1]),               % try different offsets (upper,middle,lower)
+                           member(Dy,[-1,0,1]),               % try different Y-offsets (upper,middle,lower)
                            Ny is Y+Dy,                        % Neighbors Y coordinate
-                           (Nx \= X; Ny \= Y).              % avoid (Nx,Ny)=(X,Y) 
+                           (Nx \= X; Ny \= Y).                % avoid (Nx,Ny)=(X,Y) aka Dx=Dy=0
 
-%
-friendinsight(X, Y, Dx, Dy, FriendlyColor, Board) :-  nb(X,Y, Nx, Ny, Dx, Dy), member((FriendlyColor, Nx, Ny), Board). % found friend in sight                            
-friendinsight(X, Y, Dx, Dy, FriendlyColor, Board) :-  nb(X,Y, Nx, Ny, Dx, Dy), 
-                                                      swap(FriendlyColor, Enemy),member((Enemy, Nx, Ny), Board),   % found enemy in row  
-                                                      friendinsight(Nx,Ny,Dx,Dy,FriendlyColor, Board).       % Continue searching
-%friendinsight(_, _, _ , _ , _            , _) :- fail.                                                  % not used field, fail!                     
-                           
-% 
-neighbor(Color, Board, X, Y) :- swap(Color, Enemy), 
-                                nb(X,Y, Nx, Ny, Dx, Dy),         % check neighbors
-                                member((Enemy, Nx, Ny), Board),  % one neighbor has to be the other color
-                                friendinsight(Nx, Ny, Dx, Dy, Color, Board).  % and behind it we have to find after more enemies, a friendly stone
+% go in a direction hopping over enemies, until you encounter a friend
+friendinsight(X, Y, Dx, Dy, FriendlyColor, Board) :-  nb(X,Y, Nx, Ny, Dx, Dy),                % get neighbor coordinates in same dir
+                                                      member((FriendlyColor, Nx, Ny), Board). % found friend in sight                            
+friendinsight(X, Y, Dx, Dy, FriendlyColor, Board) :-  swap(FriendlyColor, EnemyColor),
+                                                      nb(X,Y, Nx, Ny, Dx, Dy),                          % get neighbor coordinates in same dir
+                                                      member((EnemyColor, Nx, Ny), Board),              % found enemy in row  
+                                                      friendinsight(Nx,Ny,Dx,Dy,FriendlyColor, Board).  % hop on it, ontinue searching
 
                                 
 % Define a predicate legalmove(Color, Board, X, Y), that given a color, a board, and
 % a postion, succeeds if they constitute a legal move according to the rules of Reversi. Observe
 % that this predicate is useful both for finding coordinates and for determining if a particular
 % coordinate constitutes a legal move. I.e. X and Y free (output) or bound (input)
-legalmove(Color, Board, X, Y) :- posempty(Color, Board, X, Y), neighbor(Color, Board, X, Y).
+legalmove(Color, Board, X, Y) :-  posempty(Color, Board, X, Y), 
+                                  swap(Color, EnemyColor), 
+                                  nb(X,Y, Nx, Ny, Dx, Dy),         % check neighbors
+                                  member((EnemyColor, Nx, Ny), Board),  % one neighbor has to be the other color
+                                  friendinsight(Nx, Ny, Dx, Dy, Color, Board).  % and behind it we have to find after more enemies, a friendly stone
 
-
-
-%?- legalmove(black, [(white,d,4),(black,e,4),(black,d,5),(white,e,5)], X,Y).
-% friendinsight(d, 4, 1, 0, black, [(white,d,4),(black,e,4),(black,d,5),(white,e,5)]).
-
-
+% todo
 %Define a predicate makemove(+Color, +Board, +X, +Y, -NewBoard), that computes
 %the new board given a color, a board, and the x and y coordinates of the move. The
 %goal should fail if the move is illegal.
+makemove(+Color, +Board, +X, +Y, -NewBoard).
+
+% todo
 %Also, define a predicate makemoves(+Color, +Board, +N, -Moves, -NewBoard)
 %that computes a new board and a list of moves leading to that board given a starting color,
 %a board and the number of moves to be made. Observe that makemove might fail due to
@@ -62,8 +60,9 @@ legalmove(Color, Board, X, Y) :- posempty(Color, Board, X, Y), neighbor(Color, B
 %the list of moves and the no-move should count as any other move. However, the no-move
 %shall NOT be a possible move if there exists a legal move. As you might already have understood,
 %the makemoves predicate will never fail.
-makemove(+Color, +Board, +X, +Y, -NewBoard).
+makemoves(+Color, +Board, +N, -Moves, -NewBoard).
 
+% todo
 %Determining the best possible move in Reversi is quite hard. However, we can make a
 %good approximation based on a predicate valueof(+Color,+Board,-Value), that
 %computes a value of a given board for a certain color. This predicate can be very sophisticated
@@ -74,6 +73,7 @@ makemove(+Color, +Board, +X, +Y, -NewBoard).
 %edges two times, more than regular positions. Feel free to try out different weights.
  valueof(+Color,+Board,-Value).
 
+% todo
 %Define a predicate findbestmove(+Color, +Board, +N, -X, -Y) that computes
 %the “best” position to put a piece for the given color, given a board and the number moves
 %it should look ahead. In this predicate you are supposed to use the findall predicate to
