4b6344b682ead2d08713d4c3f999f8cfd0617256
nitzel <jan.s.92@web.de>
2015-10-19 00:00:34 +0200

    dnsname instead of ip, worx!

--- network-programming/lab4/attacker/startmainnode.sh
+++ network-programming/lab4/attacker/startmainnode.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #to start the JADE GUI and the Overseer
 #has to be starte 
+HOST="ec2-52-19-100-57.eu-west-1.compute.amazonaws.com"
 PORT=4321
 DFMAX="-jade_domain_df_maxresult 100000" # maximum results for searches
 OBJ=Gru:lab4.Overseer
-java jade.Boot -gui -local-port $PORT $DFMAX $OBJ
+java jade.Boot -gui -local-port $PORT -local-host $HOST $DFMAX $OBJ
 
--- network-programming/lab4/attacker/startspawnernode.sh
+++ network-programming/lab4/attacker/startspawnernode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SID=$RANDOM
-HOST=52.19.100.57
+HOST="ec2-52-19-100-57.eu-west-1.compute.amazonaws.com"
 PORT=4321
 OBJ=Spanner$SID:lab4.MinionSpawner
 java jade.Boot -container -container-name $SID -port $PORT -host $HOST  $OBJ
