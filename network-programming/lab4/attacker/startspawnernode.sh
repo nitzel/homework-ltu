eb3daf0a4e1173a33ccaef28d6137473f113c321
nitzel <jan.s.92@web.de>
2015-10-19 03:43:24 +0200

    renamed spawner

--- network-programming/lab4/attacker/startspawnernode.sh
+++ network-programming/lab4/attacker/startspawnernode.sh
@@ -2,5 +2,5 @@
 SID=$RANDOM
 HOST="ec2-52-19-100-57.eu-west-1.compute.amazonaws.com"
 PORT=4321
-OBJ=Spanner$SID:lab4.MinionSpawner
+OBJ=Spawner$SID:lab4.MinionSpawner
 java -Xmx1g jade.Boot -container -container-name $SID -port $PORT -host $HOST  $OBJ
